20c10335d1c671964ac476802f1ede589817dc9a
Cheng-Yang Fu <cyfu@cs.unc.edu>
2018-11-11 21:47:20 -0500

    Make the adjust_smooth_l1_loss run on each channels separately.

--- maskrcnn_benchmark/layers/adjust_smooth_l1_loss.py
+++ maskrcnn_benchmark/layers/adjust_smooth_l1_loss.py
@@ -1,6 +1,8 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 import torch
 from torch import nn
+import logging
+from torch.distributed import deprecated as dist
 
 class AdjustSmoothL1Loss(nn.Module):
 
@@ -13,20 +15,29 @@ class AdjustSmoothL1Loss(nn.Module):
             'running_mean', torch.empty(num_features).fill_(beta)
         )
         self.register_buffer('running_var', torch.zeros(num_features))
+        self.logger = logging.getLogger("maskrcnn_benchmark.trainer")
 
     def forward(self, inputs, target, size_average=True):
 
         n = torch.abs(inputs -target)
         with torch.no_grad():
+            self.running_mean = self.running_mean.to(n.device)
             self.running_mean *= (1 - self.momentum)
-            self.running_mean += (self.momentum * n.mean())
+            self.running_mean += (self.momentum * n.mean(dim=0))
+            self.running_var = self.running_var.to(n.device)
             self.running_var *= (1 - self.momentum)
-            self.running_var += (self.momentum * n.var())
+            self.running_var += (self.momentum * n.var(dim=0))
 
-        beta = (self.running_mean - self.running_var).clamp(
-            max=self.beta, min=1e-3)
+        beta = (self.running_mean - self.running_var)
+        self.logger.info('AdjustSmoothL1: {:.3}, {:.3}, {:.3}, {:.3}'.format(
+            beta[0].item(), beta[1].item(), beta[2].item(), beta[3].item()
+        ))
+        beta = beta.clamp(max=self.beta, min=1e-3)
+
+        #beta = (self.running_mean - self.running_var).clamp(
+        #    max=self.beta, min=1e-3)
+
         beta = beta.view(-1, self.num_features).to(n.device)
-        print(beta)
         cond = n < beta.expand_as(n)
         loss = torch.where(cond, 0.5 * n ** 2 / beta, n - 0.5 * beta)
         if size_average:
